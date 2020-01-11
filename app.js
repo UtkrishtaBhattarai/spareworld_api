e0643217e33307a54e040bcfe469c818cb1bc986
utkrishtabhattarai <bhattarai.utk1@gmail.com>
2020-01-11 23:32:41 +0545

    Some Changes

---
+++ app.js
@@ -0,0 +1,11 @@
+const bodyParser=require('body-parser');
+const express=require('express');
+var app=express();
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+require('./db/spareparts');
+const registerRoute=require('./routes/register_route');
+app.use(express.json());
+app.use(registerRoute);
+app.listen(3000);
--- db/spareparts.js
+++ db/spareparts.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/spare-api',{
+mongoose.connect('mongodb://127.0.0.1:27017/spare_api',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true
